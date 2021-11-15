8009753ea5db05ca62681afe8ac3b1357b6c199e
ShoukkiyaAshraf <shoukkiya59@gmail.com>
2021-11-15 20:39:41 +0530

    added ipnyb file

--- main.py
+++ main.py
@@ -19,4 +19,14 @@ raw = f.read().lower()
 sent_tokens = nltk.sent_tokenize(raw) #converts to lines/sentences
 word_tokens = nltk.word_tokenize(raw) #convets to words
 
+# Normalizing tokens
 
+lemmer = nltk.stem.WordNetLemmatizer()
+
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
